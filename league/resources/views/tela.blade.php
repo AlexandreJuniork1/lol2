37b261c133fd9b81326557be95f9197d61063e7d
alexandrejuniork1 <alexandrejuniork1@gmail.com>
2022-04-13 10:18:28 -0300

    second commit

--- league/resources/views/tela.blade.php
+++ league/resources/views/tela.blade.php
@@ -67,12 +67,74 @@
         </div>
     </header>
     {{---CORPO DO SITE--}}
+    {{---CORPO CIMA--}}
     <div class="corpo-inteiro">
         <div class=corpo-cima>
             <div class="corpo-cima-cima">
                 <img id="borrado-cima" src="{{asset('/assets/imgs/fundo_de_cima.jpg')}}" alt="homepage">
+                
                 <img id="borrado-fundo" src="{{asset('/assets/imgs/borrado-fundo.jpg')}}" alt="borrado">
+                
                 <img id="logo-league"src="{{asset('/assets/imgs/Logo_league_of_legends.png')}}" alt="">
+
+                <input type="submit" value="JOGUE DE GRAÇA" name="jogar" id="jogar">
+            </div>
+        </div>
+        {{---CORPO NOTICIAS--}}
+        <div class="corpo-noticias">
+            <div class="pl-noticias">
+                NOTÍCIAS EM DESTAQUE ---
+            </div>
+            <div class="imagem1-noticias">
+                <div class="imagem-NED1">
+                    <img id="NED1"src="{{asset('/assets/imgs/NED_1.jpg')}}" alt="trailer">
+                </div>
+                <div class="pl-NED1">
+                    <div id="pl-at1">
+                        <span>Atualizações do jogo</span>
+                    </div>
+                    <div id="pl-trailer">
+                        <span>Trailer do Evento <br> Esquadrão Anima</span>
+                    </div>
+                    <div id="pl-vista">
+                        <span>Vista o seu traje e se jogue</span>
+                    </div>
+                </div>  
+            </div>      
+            <div class="imagem2-noticias">
+                    <div class="imagem-NED2">
+                         <img id="NED2" src="{{asset('/assets/imgs/NED_2.jpg')}}" alt="torneio">
+                    </div> 
+                    <div class="pl-NED2">   
+                         <div class="pl-comu">
+                            <span>Comunidade</span>
+                         </div>
+                         <div class="pl-torne">
+                            <span>Torneios de <br>Comunidade - League<br> of Legends - 12/04 a</span>
+                         </div>
+                    </div>
+            </div>
+            <div class="imagem3-noticias">
+                    <div class="imagem-NED3">
+                        <img id="NED3" src="{{asset('/assets/imgs/NED_3.jpg')}}" alt="dev">
+                    </div>
+                    <div class="pl-NED3">
+                        <div class="pl-dev">
+                            <span>DEV</span>
+                        </div>
+                        <div class="pl-vis">
+                            <span>Visões de<br> Balanceamento 08/04: <br> os Desafios vêm por ai</span>
+                        </div>
+                    </div>
+            </div>
+            <div class="exibir">
+                <a href="">Exibir Tudo -> </a>
+            </div>
+        </div>
+        {{---CORPO-IMAGEM-MEIO--}}
+        <div class="img-meio">
+            <div class="img-meio-meio">
+                <img id="img-runeterra"src="{{asset('/assets/imgs/home_page_centro.jpg')}}" alt="">
             </div>
         </div>
     </div>
